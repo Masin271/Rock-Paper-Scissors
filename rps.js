043822cf78893b702162b85b5193c00d28245a8f
Masin271 <masinmasin271@gmail.com>
2024-09-29 19:08:01 +0330

    I used biome extension

--- rps.js
+++ rps.js
@@ -1,71 +1,58 @@
 let humanScore = 0;
 let computerScore = 0;
 function getComputerChoice(a) {
-    a = Math.random();
-    if (a > 0 && a <= 0.33) {
-        console.log("rock");
-        return "rock";
-    }
-    else if (a > 0.33  && a <= 0.66) {
-        console.log("paper");
-        return "paper";
-    }
-    else  {
-        console.log("scissors");
-        return "scissors";
-    }
+  a = Math.random();
+  if (a > 0 && a <= 0.33) {
+    console.log("rock");
+    return "rock";
+  } else if (a > 0.33 && a <= 0.66) {
+    console.log("paper");
+    return "paper";
+  } else {
+    console.log("scissors");
+    return "scissors";
+  }
 }
-
 
 function getHumanChoice(b) {
-    b = prompt("Your turn: rock, paper or scissors?").trim().toLowerCase();
-    return b;
+  b = prompt("Your turn: rock, paper or scissors?").trim().toLowerCase();
+  return b;
 }
 
-
-
-
-
-
 function playGame() {
-    function playRound(humanSelection, computerSelection) {
-        if ((humanSelection == "rock" && computerSelection == "scissors") || (humanSelection == "scissors" && computerSelection == "paper") || (humanSelection == "paper" && computerSelection == "rock")) {
-            ++humanScore;
-            alert("You won!")
-        }
-        else if ((humanSelection == "rock" && computerSelection == "paper") || (humanSelection == "scissors" && computerSelection == "rock") || (humanSelection == "paper" && computerSelection == "scissors")) {
-            ++computerScore;
-            alert("You lost!")
-        }
-        else if (humanSelection == computerSelection) {
-            ++computerScore;
-            ++humanScore;
-            alert("You drew!")
-        }
-        else {
-            console.log("Bug");
-        }
+  function playRound(humanSelection, computerSelection) {
+    if (
+      (humanSelection == "rock" && computerSelection == "scissors") ||
+      (humanSelection == "scissors" && computerSelection == "paper") ||
+      (humanSelection == "paper" && computerSelection == "rock")
+    ) {
+      ++humanScore;
+      alert("You won!");
+    } else if (
+      (humanSelection == "rock" && computerSelection == "paper") ||
+      (humanSelection == "scissors" && computerSelection == "rock") ||
+      (humanSelection == "paper" && computerSelection == "scissors")
+    ) {
+      ++computerScore;
+      alert("You lost!");
+    } else if (humanSelection == computerSelection) {
+      ++computerScore;
+      ++humanScore;
+      alert("You drew!");
+    } else {
+      console.log("Bug");
     }
-    for (let x = 0; x < 5; x++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        alert("You won the game!")
-    }
-    else if (humanScore < computerScore) {
-        alert("You lost the game!")
-    }
-    else {
-        alert("You drew the game")
-    }
+  }
+  for (let x = 0; x < 5; x++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  if (humanScore > computerScore) {
+    alert("You won the game!");
+  } else if (humanScore < computerScore) {
+    alert("You lost the game!");
+  } else {
+    alert("You drew the game");
+  }
 }
 
 playGame();
-
-
-
-
-
-
-
-
